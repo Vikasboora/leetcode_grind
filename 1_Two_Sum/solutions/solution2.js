6f60302c5a48253341325975f35442594279c4c1
Vikas Kumar <vikaskumar077777@gmail.com>
2025-12-18 22:13:16 +0530

    solution 2 using map

--- 1_Two_Sum/solutions/solution2.js
+++ 1_Two_Sum/solutions/solution2.js
@@ -1,6 +1,7 @@
 let nums = [2, 7, 11, 15];
 let target = 18;
 
+// Solution Using Map
 var solution2 = function (nums, target) {
   const map = new Map();
   for (let i = 0; i < nums.length; i++) {
@@ -13,3 +14,17 @@ var solution2 = function (nums, target) {
 };
 
 console.log(solution2(nums, target));
+
+// Solution Using Object
+var solution_2 = function (nums, target) {
+  const object = {};
+  for (let i = 0; i < nums.length; i++) {
+    let secondNumber = target - nums[i];
+    if (object.hasOwnProperty(secondNumber)) {
+      return [i, object[secondNumber]];
+    }
+    object[nums[i]] = i;
+  }
+};
+
+console.log(solution_2(nums, target));
