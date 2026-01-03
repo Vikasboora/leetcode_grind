43d5bf5bf6f34ac5c26b80d65d977bcf5b4e59ca
Vikas Kumar <vikaskumar077777@gmail.com>
2026-01-03 19:22:46 +0530

    sol 3

--- 5_Longest_Palindromic_Substring/solutions/solution3.js
+++ 5_Longest_Palindromic_Substring/solutions/solution3.js
@@ -1,5 +1,40 @@
-let str = 'babad';
+let str = 'bancana';
 
-function solution3(s){
+function solution3(s) {
+  if (s.length < 2) return s;
 
+  let start = 0;
+  let maxLength = 1;
+
+  // Helper function to expand around a center
+  function expandAroundCenter(left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      left--;
+      right++;
+    }
+    // When loop exits, left & right are 1 step beyond the palindrome
+    return right - left - 1; // palindrome length
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    // Odd length palindrome (center at i)
+    let len1 = expandAroundCenter(i, i);
+
+    // Even length palindrome (center between i and i+1)
+    let len2 = expandAroundCenter(i, i + 1);
+
+    let currMax = Math.max(len1, len2);
+
+    if (currMax > maxLength) {
+      maxLength = currMax;
+
+      // Compute the new start index
+      start = i - Math.floor((currMax - 1) / 2);
+      console.log(i);
+    }
+  }
+
+  return s.substring(start, start + maxLength);
 }
+
+console.log(solution3(str));
