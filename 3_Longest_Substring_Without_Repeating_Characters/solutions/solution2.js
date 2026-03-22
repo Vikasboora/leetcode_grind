f1211c2e7a4e1f2b022ae75102391350b671755d
Vikas Kumar <vikaskumar077777@gmail.com>
2026-03-22 14:31:34 +0530

    added the solution when i have tried 2nd time

--- 3_Longest_Substring_Without_Repeating_Characters/solutions/solution2.js
+++ 3_Longest_Substring_Without_Repeating_Characters/solutions/solution2.js
@@ -16,3 +16,33 @@ function solution2(s) {
 }
 
 console.log(solution2(str));
+
+
+
+function solution3(s) {
+    let map = new Map();
+    let left = 0;
+    let maxLength = 0;
+
+    for(let right = 0; right < s.length; right++){
+        let char = s[right];
+
+        if (map.has(char)){
+            map.set(char, (map.get(char)+ 1))
+        }
+        else {
+            map.set(char,1);
+        }
+
+        while (map.get(char) > 1){
+            let leftside = s[left];
+            map.set(leftside, map.get(leftside)-1);
+            left++;
+        }
+
+        maxLength = Math.max(maxLength, right-left+1);
+    }
+    return maxLength;
+}
+
+console.log(solution3(str));
